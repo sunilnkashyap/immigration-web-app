514e41c3cfe290cc2c614a0aa18749f4090b034a
Sunil Kashyap <sunilnkashyap@gmail.com>
2024-06-20 11:45:05 +0530

    fix traslation layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import React, { StrictMode, Suspense, useEffect } from "react";
-import { RouterProvider } from "react-router-dom";
-import { appRouter } from "./routes";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./theme";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import React, { StrictMode, Suspense, useEffect } from "react";
+
+import CssBaseline from "@mui/material/CssBaseline";
+import { RouterProvider } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { ToastContainer } from "react-toastify";
+import { appRouter } from "./routes";
+import { theme } from "./theme";
 import { useTranslation } from "react-i18next";
 
 function App() {
   const { t, i18n } = useTranslation();
   return (
-    <div className={`${i18n.language == "en" ? "" : "ta_r"}`}>
+    <div className={`${i18n.language == "ar" ? "ta_r" : ""}`}>
       <Suspense fallback={<div>Loading...</div>}>
         <StrictMode>
           <CssBaseline />
